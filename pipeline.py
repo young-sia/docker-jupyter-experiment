ed01676f5b3a8eb3721b1fb36e50b1d4fcf8990b
young-sia <youngly1028@gmail.com>
2022-05-07 17:53:24 +0900

    make many tables in database

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,6 @@
+import json
 import os
+import urllib.parse
 
 import prefect
 from prefect import task, Flow
@@ -14,7 +16,8 @@ def retrieve_tweets():
     response = requests.get(
         'https://api.twitter.com/2/tweets/search/recent',
         params={
-            'query': 'data science',
+            'query': '"data science" has:links',
+            'tweet.fields': 'entities',
             'max_results': '100',
         },
         headers={
@@ -22,7 +25,7 @@ def retrieve_tweets():
         },
     )
     results = response.json()
-    logger.info(f'data:{results}')
+    logger.info(f'data:{json.dumps(results)}')
     logger.info(f'got {len(results["data"])} tweets')
     return results
 
@@ -31,9 +34,26 @@ def retrieve_tweets():
 def transform_data(response):
     logger = prefect.context.get("logger")
     logger.info('transforming tweet data')
-    data = [[tweet['id'], tweet['text']] for tweet in response['data']]
-    logger.info(f'reformatted {len(data)} records')
-    return data
+    hostname_counts = {}
+    for tweet in response['data']:
+        if 'urls' in tweet['entities']:
+            for url in tweet['entities']['urls']:
+                if 'unwound_url' in url:
+                    hostname = urllib.parse.urlparse(url['unwound_url']).hostname
+                else:
+                    hostname = urllib.parse.urlparse(url['expanded_url']).hostname
+
+                # this hostname has been seen before
+                if hostname in hostname_counts:
+                    # so we increase its count by 1
+                    hostname_counts[hostname] += 1
+                # this hostname has not been seen before
+                else:
+                    # this is the first time seeing it, so set the count to 1
+                    hostname_counts[hostname] = 1
+    logger.info(f'reformatted {len(hostname_counts)} records')
+    logger.info(f'hostname_counts: {json.dumps(hostname_counts)}')
+    return hostname_counts
 
 
 @task(log_stdout=True)
@@ -47,7 +67,7 @@ def store_data(records):
         database="twitter"
     )
     cursor = connection.cursor()
-    sql = 'insert into tweet(id, content) values (%s, %s)'
+    sql = 'insert into tweet(id,id_tweet, api_tweet, content) values (%s, %s, %s, %s)'
     logger.info('inserting tweet data')
     # executemany has an optimization for inserts where it converts multiple
     # individual insert statements into a multi-record insert
